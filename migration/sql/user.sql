af8eeb5d9756283a50189ec67c72093b0121a407
biFebriansyah <bukanebi@gmail.com>
2023-06-09 18:05:35 +0700

    change user schema

--- migration/sql/user.sql
+++ migration/sql/user.sql
@@ -3,6 +3,7 @@ CREATE TABLE public.users (
 	username varchar NOT NULL,
 	"password" varchar NOT NULL,
 	email varchar NULL,
+	"role" varchar NULL DEFAULT 'user',
 	created_at timestamp without time zone NULL DEFAULT now(),
 	updated_at timestamp without time zone NULL,
 	CONSTRAINT users_pk PRIMARY KEY (user_id),
